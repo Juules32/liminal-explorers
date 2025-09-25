bbbb898ba5305eaf1d2c2bdf0cf8c30cef0a2972
juules32 <56916699+Juules32@users.noreply.github.com>
2025-09-25 22:44:05 +0200

    .

--- globals/NetworkManager.gd
+++ globals/NetworkManager.gd
@@ -102,6 +102,7 @@ func _connect_peer_to_lobby(peer_id: int) -> void:
 	player_instance.player_id = peer_id
 	player_instance.name = str(peer_id)
 	player_instance.position = Vector2(400, 400)
+	player_instance.connect_enter_tree(peer_id)
 	get_node("/root/World").add_child(player_instance)
 
 func _disconnect_peer_from_lobby(peer_id: int) -> void:
