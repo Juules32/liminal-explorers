679ddbf07342149d24dc5eb3b41ab7277cf01011
juules32 <56916699+Juules32@users.noreply.github.com>
2025-09-25 23:56:19 +0200

    fixed

--- player.gd
+++ player.gd
@@ -4,16 +4,19 @@ extends CharacterBody2D
 @onready var label: Label = $Label
 @onready var input_synchronizer: MultiplayerSynchronizer = $InputSynchronizer
 
-@export var player_id: int = -1
+@export var player_id: int = -1:
+	set(id):
+		player_id = id
+		%InputSynchronizer.set_multiplayer_authority(id)
 
 const SPEED: float = 500.0
 
 func connect_enter_tree(pid: int) -> void:
 	%InputSynchronizer.tree_entered.connect(_on_input_enter_tree.bind(pid))
 
-func _on_input_enter_tree(pid: int) -> void:
+func _on_input_enter_tree(_pid: int) -> void:
 	print("tttee")
-	%InputSynchronizer.set_multiplayer_authority(pid)
+	# %InputSynchronizer.set_multiplayer_authority(pid)
 
 func _ready() -> void:
 	pass
