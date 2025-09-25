274b9aff1e70c15c2f293aa58296b2ca56f8c046
juules32 <56916699+Juules32@users.noreply.github.com>
2025-09-25 21:46:26 +0200

    .

--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 @onready var label: Label = $Label
+@onready var input_synchronizer: MultiplayerSynchronizer = $InputSynchronizer
 
 var player_id: int = -1
 
@@ -10,12 +11,12 @@ const SPEED: float = 500.0
 @export var axis_x: float = 0
 @export var axis_y: float = 0
 
-func _init() -> void:
-	pass#set_multiplayer_authority(multiplayer.multiplayer_peer.get_unique_id())
+func _enter_tree() -> void:
+	input_synchronizer.set_multiplayer_authority(player_id)
 
 func _physics_process(delta: float) -> void:
-	if not is_multiplayer_authority():
-		return
+	#if not is_multiplayer_authority():
+	#	return
 	
 	axis_x = Input.get_axis("ui_left", "ui_right")
 	axis_y = Input.get_axis("ui_up", "ui_down")
